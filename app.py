ce436541a031c5a97044bfad6f56213846c5543d
Thevindu wijayasinghe <zerothezenixwzp@gmail.com>
2026-06-09 00:04:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-import os
+import sys
+try:
+    import cv2
+except ImportError:
+    # Force mock references if the system searches for missing display libraries
+    import collections
+    sys.modules['cv2'] = collections.defaultdict(lambda: None)
+
 import streamlit as st
 import pandas as pd
 import numpy as np
+import streamlit as st
 import cv2
 import json
 import math
